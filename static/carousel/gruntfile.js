849fe852f04a000725c9c5a3ab3460f647aef297
TVPage <tvpage@TVPages-MacBook-Pro.local>
2017-01-10 12:38:13 -0800

    Adding updates to sidebar & carousel

--- static/carousel/gruntfile.js
+++ static/carousel/gruntfile.js
@@ -59,7 +59,7 @@ module.exports = function(grunt) {
 
     var exportPath = '../../layouts/partials/carousel';
     grunt.registerMultiTask('exportcss', 'Export css to partials folder', function() {
-        var exported = grunt.file.write(exportPath+'/css.html', grunt.file.read('./css/styles.css'));
+        var exported = grunt.file.write(exportPath+'/css-base.html', grunt.file.read('./css/styles.css'));
         if (exported) {
             grunt.log.ok('exported!');
         }
---
+++ static/sidebar/gruntfile.js
@@ -0,0 +1,70 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        watch: {
+            js: {
+                files: ['js/**/*.js'],
+                tasks: ['requirejs']
+            },
+            css: {
+                files: ['sass/**/*.scss'],
+                tasks: ['sass', 'autoprefixer', 'exportcss']
+            }
+        },
+        requirejs: {
+            compile: {
+                options: {
+                    mainConfigFile: 'build.js'
+                }
+            }
+        },
+        sass: {
+          dev: {
+            options: {
+              sourceMap: true,
+              outputStyle: 'expanded',
+              includePaths: ['node_modules/bootstrap-sass/assets/stylesheets']
+            },
+            files: {
+              'css/styles.css': 'sass/styles.scss'
+            }
+          },
+          dist: {
+            options: {
+              sourceMap: true,
+              outputStyle: 'compressed',
+              includePaths: ['node_modules/bootstrap-sass/assets/stylesheets']
+            },
+            files: {
+              'css/styles.css': 'sass/styles.scss'
+            }
+          }
+        },
+        autoprefixer: {
+            css: {
+                files: {
+                    'css/styles.css': 'css/styles.css'
+                }
+            }
+        },
+        exportcss: {
+            target: {}
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    var exportPath = '../../layouts/partials/sidebar';
+    grunt.registerMultiTask('exportcss', 'Export css to partials folder', function() {
+        var exported = grunt.file.write(exportPath+'/css-base.html', grunt.file.read('./css/styles.css'));
+        if (exported) {
+            grunt.log.ok('exported!');
+        }
+    });
+    
+    grunt.registerTask('dev', ['watch']);
+
+};
