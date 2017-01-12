1a0f8110d49ebefe4b1f10c11e8548e64f991025
TVPage <tvpage@TVPages-MacBook-Pro.local>
2017-01-12 12:15:17 -0800

    Add carousel updates

--- static/carousel/js/index.js
+++ static/carousel/js/index.js
@@ -3,12 +3,47 @@ define(function(require) {
   var $ = require('jquery-private');
   require('./jquery.pubsub-loader');
   require('slick');
+  
+  var 
+  redefine = function(obj, prop){
+    return 'undefined' !== typeof obj[ prop ];
+  };
 
-  console.log( "HAHAH" );
+  //For each widget present in the page.
+  $( 'div[id^="tvpwidget"]' ).attr('id',function(i,id){
 
-  // var redefine = function(obj, prop){
-  //   return "undefined" !== typeof obj[ prop ];
-  // };
+    //Dynamic retrieval of configuration object.
+    var config = {};
+    if ( redefine(window,'__TVPage__') && redefine(__TVPage__,'config') ) {
+      for (var i = 0; i < __TVPage__.config.length; i++) {
+        if (id === __TVPage__.config[i].id) config = __TVPage__.config[i];
+      }
+    }
+
+    //Need to parse config to check if we need to load the data with AJAX or if content is already within the 
+    //code-snippet
+
+
+    // if ($.isEmpty(config) || !redefine(config,'attributes') || $.isEmptyObject(config.attributes) || !redefine(config.attributes,'search') || ) {
+
+    // }
+
+    // console.log( !redefine(config,'attributes') || $.isEmptyObject(config.attributes) );
+    // console.log( !redefine(config.attributes,'search') || !config.attributes.search )
+
+    //console.log( !redefine(config,'attributes') || $.isEmptyObject(config.attributes) );
+
+    // if ($.isEmpty(config) || ( redefine(config,'attributes') &&  $.isEmpty(config.attributes)) ) {
+
+    // } else {
+
+    // }
+
+    //  else if (redefine(config, 'attributes')) {
+
+    // }
+
+  });
 
   // var apiBase = "//localhost:1313/tvpwidget/";
   // $( "div[id^='tvpwidget']" ).attr("id",function(i,id){
