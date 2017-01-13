523ae306e0b60b225ff32cf82b0a3965120127d4
sdorle <sdorle@tvpage.com>
2017-01-13 14:16:14 -0800

    first commit for the new branch

--- static/solo/src/js/index.js
+++ static/solo/src/js/index.js
@@ -2,26 +2,31 @@ define(function(require) {
   var $ = require('jquery-private');
   var player = require('./player/index');
   var config = __TVPage__.config[0];
-  var referenceIds = config.attributes.referenceIds;
+
+  // var referenceIds = config.attributes.referenceIds;
+  // $.ajax({
+  //   url: location.protocol + "//app.tvpage.com/api/videos/referenceIds",
+  //   dataType: 'jsonp',
+  //   data: {'X-login-id': config.loginid, 'ids': referenceIds.toString() }
+  // }).done(function(res){
+  //   var videos = [];
+  //   for(var i in referenceIds){
+  //     videos = videos.concat(res[referenceIds[i]]);
+  //   }
+  //   player.init(videos,function(){});
+  // })
+
+  var obj = {};
+  var code = config.attributes.category[0].code;
+  obj['X-login-id'] = config.loginid;
+  obj[code] = config.attributes.category[0].value;
 
   $.ajax({
-    url: location.protocol + "//app.tvpage.com/api/videos/referenceIds",
+    url: location.protocol + "//app.tvpage.com/api/videos/search",
     dataType: 'jsonp',
-    data: {'X-login-id': config.loginid, 'ids': referenceIds.toString() }
+    data:obj
   }).done(function(res){
-    var videos = [];
-    for(var i in referenceIds){
-      videos = videos.concat(res[referenceIds[i]]);
-    }
-    player.init(videos,function(){});
+    console.log(res);
+    player.init(res,function(){});
   })
-
-  // $.ajax({
-  //   url: location.protocol + "//app.tvpage.com/api/videos/search",
-  //   dataType: 'jsonp',
-  //   data: {'X-login-id': config.loginid, ''+config.attributes.category[0].value+'': config.attributes.category[0].value }
-  // }).done(function(res){
-  //   console.log(res);
-  //   player.init(res,function(){});
-  // })
 });
