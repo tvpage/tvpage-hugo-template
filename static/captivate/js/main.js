27113f65e356bc3fc1e4f87b24696a7ad42aee8e
eamurao <eamurao@tvpage.com>
2017-01-19 03:00:32 -0800

    get update from CS-3088-captivate

--- static/captivate/js/main.js
+++ static/captivate/js/main.js
@@ -1,4 +1,6 @@
-(function($){
+(function($, IScroll){
+	var liveResultsPage,
+		resultsScroller;
     var formatDate = function(unixTimestamp) {
         var months = ['January','February','March','April','May','June','July','August','September','October','November','December'],
         	d = (new Date(Number(unixTimestamp) * 1000)),
@@ -32,18 +34,72 @@
     	video : function(){
 
     	},
-    	videos : function(channelId, query){
-    		var url = "//app.tvpage.com/api/videos/search";
+    	videos : function(channelId, page, query){
+    		var url = TVSite.apiUrl+"videos/search";
     		var search = query;
     		if(channelId !== undefined && channelId !== null)
-    			url = "//app.tvpage.com/api/channels/" + channelId + "/videos";
-    		return this.commonRequest(url,null,search);
+    			url = TVSite.apiUrl + channelId + "/videos";
+    		return this.commonRequest(url,page,search);
     	}
 
     };
 
     var renderUtil = {
     	liveResultHtml : '<li><a href="{url}" class="desktop-search-results-item clearfix"><div class="desktop-search-results-item-img-holder"><div class="desktop-search-results-item-img" style="background-image:url({asset.thumbnailUrl});+    	background-position: 50% 50%;"><img src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" /></div></div><div class="desktop-search-results-item-text"><p class="desktop-search-results-item-title">{title}</p><p class="desktop-search-results-item-description">{description}</p></div></a></li>',
+    	getResultUrl :function(result) {
+            var redefine = function(val) {
+                return ("undefined" !== typeof val && null !== typeof val && val);
+            };
+            if (result && redefine(result, "entityTitleParent") && redefine(result, "titleTextEncoded") && redefine(result, "entityIdParent") && redefine(result, "id")) {
+                return TVSite.baseUrl + '/' + String(result.entityTitleParent).replace(/\s/g,"-") + "/" + String(result.titleTextEncoded).replace(/\s/g,"-") + "/" + result.entityIdParent + "-" + result.id;
+            }
+        },
+    	resetLiveSearch : function() {
+            $('#desktop-search-results ul')
+                .empty()
+                .closest('#desktop-search-results-holder')
+                .hide();
+        },
+        showEndOfResults : function() {
+            if (!$('.end-of-results').length) {
+                $('<p></p>')
+                    .addClass('end-of-results')
+                    .html('No more results')
+                    .appendTo('#desktop-search-results ul');
+                this.checkResultsScroller();
+            }
+        },
+        createDesktopScroller : function(id) {
+            if (id && $(id).children().length) {
+                return new IScroll(id, {
+                    interactiveScrollbars: true,
+                    scrollX: false,
+                    click: true,
+                    mouseWheel: true,
+                    scrollbars: true
+                });
+            }
+        },
+        handleScrollEnd : function() {
+            if (Math.abs(this.maxScrollY) - Math.abs(this.y) < 10) {
+                var val = $('#desktop-search-input').val();
+                channelDataExtractor.videos(null, liveResultsPage + 1,val)
+                    .done(function(results) {
+                        renderUtil.handleVideoResults(results);
+                        liveResultsPage = liveResultsPage + 1;
+                    });
+            }
+        },
+        checkResultsScroller : function() {
+            if (!this.resultsScroller) {
+                this.resultsScroller = this.createDesktopScroller('#desktop-search-results-holder');
+                this.resultsScroller.on('scrollEnd', this.handleScrollEnd);
+            } else {
+                setTimeout(function() {
+                    renderUtil.resultsScroller.refresh();
+                }, 0);
+            }
+        },
     	tmpl : function(template, data) {
             if (template && 'object' == typeof data) {
                 return template.replace(/\{([\w\.]*)\}/g, function(str, key) {
@@ -65,7 +121,8 @@
                     holder = '#desktop-search-results-holder';
                 for (var i = 0; i < results.length; ++i) {
                     var result = results[i];
-                    //result['url'] = getResultUrl(result);
+                    //debugger;
+                    result['url'] = this.getResultUrl(result);
                     if ('description' in result) result.description = this.stripHtml(result.description);
                     html += this.tmpl(this.liveResultHtml, result);
                 }
@@ -74,17 +131,17 @@
                     .append(html)
                     .closest(holder)
                     .show();
-                //checkResultsScroller();
+                renderUtil.checkResultsScroller();
 
             } else {
-                //showEndOfResults();
+                renderUtil.showEndOfResults();
             }
         }
     };
 
     var search = {
     	desktop : function(query){
-    		 return channelDataExtractor.videos(null, query);
+    		 return channelDataExtractor.videos(null, null, query);
 
     	},
     	mobile : function(){
@@ -104,26 +161,30 @@
     searchDesktopInput.on('keyup', function(e) {
         e.preventDefault();
         var val = $(e.target).val();
-    
+        var $nullResults = $('#null-results');
+    	renderUtil.resetLiveSearch();
         if (val) {
         	liveResultsPage = 0;
             search.desktop(val).done(function(results) {
             	if (results.length) {
+            		$nullResults.hide();
             		renderUtil.handleVideoResults(results);
 				} else {
-                
-                
+                	$nullResults.show();
                 }
             });
         } else {
-            
-            
+            $nullResults.hide();
         }
     });
     searchDesktopInput.focus(function(){
-    	$(".brand-header-search-container").animate({width:"+=180"},"medium");
+    	$(".brand-header-search-container").animate({width:"+=175"},"fast");
+    	$(".brand-header-logo").animate({marginLeft:"-=175"},"fast");
     }).blur(function(){
-    	$(".brand-header-search-container").animate({ width: '-=180' }, "medium");
+    	searchDesktopInput.val("");
+    	renderUtil.resetLiveSearch();
+    	$(".brand-header-search-container").animate({ width: '-=175' }, "fast");
+    	$(".brand-header-logo").animate({marginLeft:"+=175"},"fast");
     });
 
 	$('.slider').slick({
@@ -173,8 +234,8 @@
 	$('.video-details .published-date').text(function(i, s){
 		return formatDate(s);
 	});
-	$('#mobile-menu').on('click', function(event) {
-		event.preventDefault();
-		$('#mobile-menu-modal').modal();
-	});
-}(jQuery));
+    $('#mobile-menu').on('click', function(event) {
+        event.preventDefault();
+        $('#mobile-menu-modal').modal();
+    });
+}(jQuery, window.IScroll));
