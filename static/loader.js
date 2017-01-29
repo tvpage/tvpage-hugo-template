7ad6c18b271924451f10b904ea36cf1a69dab2a5
TVPage <tvpage@TVPages-MacBook-Pro.local>
2017-01-29 12:10:01 -0800

    Add updates

--- static/loader.js
+++ static/loader.js
@@ -3,142 +3,158 @@
 //to load the iframe scripts asynchronouse, this hides the browser spinner.
 ;(function(root,doc){
 
-  //PROFILE & DEBUG:
-  root.startTime = performance.now();
-  root.interactionReadyTime = 0;
-  root.DEBUG = 1;
+  if (root.DEBUG) {
+    root.DEBUG_start = performance.now();
+  }
 
-  var spots = doc.querySelectorAll('.tvp-sidebar, .tvp-solo'),
-      spotsCount = spots.length,
-      pre = 'tvp-iframe',
-      redefine = function(o,p){return 'undefined' !== typeof o[p]},
-      getIfr = function(){
-        var i = doc.createElement('iframe');
-        i.setAttribute('allowfullscreen', '');
-        i.classList.add(pre);
-        i.setAttribute('frameborder', '0');
-        return i;
+  var isset = function(o,p){
+        return 'undefined' !== typeof o[p]
+      },
+      appendToHead = function(el){
+        (doc.getElementsByTagName('head')[0]||doc.getElementsByTagName('body')[0]).appendChild(el);
       };
 
-function Widget(spot) {
-  var widget = function(){};
+  function Widget(spot) {
+    var widget = function(){};
 
-  //Define the data method.
-  var dataMethod = 'static';
-  var id = spot.getAttribute('data-id');
-  if (redefine(root,'__TVPage__') && redefine(__TVPage__,'config') && redefine(__TVPage__.config,id) &&
-    redefine(__TVPage__.config[id],'channel') && redefine(__TVPage__.config[id].channel,'id')) {
-    dataMethod = 'dynamic';
-  }
-  
-  var domain = spot.getAttribute('data-domain'),
-      type = id.split('-').shift();
+    var libsExt = root.DEBUG ? '.js' : '.min.js';
 
-  widget.run = function() {
-    spot.insertAdjacentHTML('beforebegin', '<div id="' + id + '-holder" class="' + pre + '-holder"></div>');
-    var holder = doc.getElementById(id + '-holder'),
-        embedMethod = spot.getAttribute('data-embedmethod') || 'iframe';
+    var dataMethod = 'static',
+        id = spot.getAttribute('data-id');
+
+    if (isset(root,'__TVPage__') && isset(__TVPage__,'config') && isset(__TVPage__.config,id) &&
+      isset(__TVPage__.config[id],'channel') && isset(__TVPage__.config[id].channel,'id')) {
+      dataMethod = 'dynamic';
+    }
     
-    if (embedMethod === 'iframe') {
-      var lazy = true, iframe = getIfr();
+    var domain = spot.getAttribute('data-domain'),
+        type = id.split('-').shift(),
+        cssLib = domain+'/'+type+'/styles.css';
 
-      holder.appendChild(iframe);
+    widget.run = function() {
+      spot.insertAdjacentHTML('beforebegin', '<div id="' + id + '-holder" class="' + pre + '-holder"></div>');
       
-      //DEBUG iframe append time
-      root.iframeAppendTime = performance.now();
+      var holder = doc.getElementById(id + '-holder'),
+          embedMethod = spot.getAttribute('data-embedmethod') || 'iframe';
+      
+      if (embedMethod === 'iframe') {
+        var lazy = true;
+        var iframe = doc.createElement('iframe');
+        iframe.setAttribute('allowfullscreen', '');
+        iframe.classList.add('tvp-iframe');
+        iframe.setAttribute('frameborder', '0');
+        holder.appendChild(iframe);
 
-      //Reference for the performance boost technique
-      //http://www.aaronpeters.nl/blog/iframe-loading-techniques-performance?%3E
-      if (lazy && 'dynamic' === dataMethod) {
-        var html = '<body class="' + dataMethod + ' is-iframe" data-domain="'+domain+'" data-id="' + id + '" data-src="' + (spot.href || '') + '" onload="'+
-        'var d = document, head = d.getElementsByTagName(\'head\')[0],'+
-        'injScr = function(sr){ var s=d.createElement(\'script\');s.src=sr;head.appendChild(s);};';
+        //Reference for the performance boost technique
+        //http://www.aaronpeters.nl/blog/iframe-loading-techniques-performance?%3E
+        if (lazy && 'dynamic' === dataMethod) {
+          var html = '<body class="' + dataMethod + ' is-iframe" data-domain="'+domain+'" data-id="' + id + '" data-src="' + (spot.href || '') + '" onload="'+
+          'var d = document, head = d.getElementsByTagName(\'head\')[0],'+
+          'injScr = function(sr){ var s=d.createElement(\'script\');s.src=sr;head.appendChild(s);};';
 
-        var libs = {tvpsolo: '\''+domain+'\/playerpack'+(root.DEBUG ? '' : '.min')+'.js\'',tvppack: '\''+domain+'\/' + type + '\/lib'+(root.DEBUG ? '' : '.min')+'.js\''},
+          var libs = {
+                tvpsolo: '\''+domain+'\/playerpack'+libsExt+'\'',
+                tvppack: '\''+domain+'\/' + type + '\/lib'+libsExt+'\'',
+                player: '\''+domain+'\/player'+libsExt+'\''
+              },
+              libsCounter = Object.keys(libs).length;
+
+          while (libsCounter > 0) {
+            var key = Object.keys(libs)[libsCounter-1];
+            html += 'injScr(' + libs[key] + ');';
+            libsCounter--;
+          }
+
+          html += 'var css=d.createElement(\'link\');css.rel=\'stylesheet\';css.type=\'text/css\';';
+          html += 'css.href='+('\''+domain+'\/' + type)+'\/styles.css\';head.appendChild(css);'
+          html += '">';
+
+          iframe.onload = function(){
+            if (root.DEBUG) console.debug("Iframe loaded at: " + (performance.now() - root.DEBUG_start) + " ms");
+          };
+          
+          var iframeDoc = iframe.contentWindow.document;
+          iframeDoc.open().write(html);
+          iframeDoc.close();
+
+        } else {
+          function setSrc() {
+            var s = spot.href,
+                ifr = holder.firstChild;
+            (-1 == navigator.userAgent.indexOf("MSIE")) ? ifr.src = s : ifr.location = s;
+          }
+          setTimeout(setSrc,0);
+        }
+      } else {
+
+        holder.classList.add('inline');
+
+        __TVPage__.inline = __TVPage__.inline || [];
+        __TVPage__.inline.push(id);
+        __TVPage__.inlineCount = __TVPage__.inline.length || 0;
+        if (! __TVPage__.inlineCount) return;
+
+        //Adding the libs to be used in the iframe.
+        var libs = {
+              tvppack: domain + '/playerpack'+libsExt,
+              tvpsolo: domain + '/'+type +'/lib'+libsExt,
+              player: domain + '/player'+libsExt
+            },
+            libsFrag = doc.createDocumentFragment(),
             libsCounter = Object.keys(libs).length;
+
         while (libsCounter > 0) {
           var key = Object.keys(libs)[libsCounter-1];
-          html += 'injScr(' + libs[key] + ');';
+          if (doc.getElementById(key)) break;
+          var scr = doc.createElement('script');
+          scr.id = 'tvpa';
+          scr.type = 'text/javascript';
+          scr.async = true;
+          scr.src = libs[key].replace(/'/g,'');
+          libsFrag.appendChild(scr);
           libsCounter--;
         }
 
-        html += 'var css=d.createElement(\'link\');css.rel=\'stylesheet\';css.type=\'text/css\';';
-        html += 'css.href='+('\''+domain+'\/' + type)+'\/styles.css\';head.appendChild(css);'
-        html += '">';
-
-        var iframeDoc = iframe.contentWindow.document;
-        iframeDoc.open().write(html);
-        iframeDoc.close();
-
-      } else {
-        function setSrc() {
-          var s = spot.href,
-              ifr = holder.firstChild;
-          (-1 == navigator.userAgent.indexOf("MSIE")) ? ifr.src = s : ifr.location = s;
-        }
-        setTimeout(setSrc,0);
+        var link = doc.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = cssLib;
+        libsFrag.appendChild(link);
+        appendToHead(libsFrag);
       }
-    } else {
-      __TVPage__.inline = __TVPage__.inline || [];
-      __TVPage__.inline.push(id);
-      __TVPage__.inlineCount = __TVPage__.inline.length || 0;
-      if (! __TVPage__.inlineCount) return;
-
-      //Adding the libs to be used in the iframe.
-      var libs = {tvpsolo: domain + '/playerpack'+(root.DEBUG ? '' : '.min')+'.js',tvppack: domain + '/'+type +'/lib'+(root.DEBUG ? '' : '.min')+'.js'},
-          libsFrag = doc.createDocumentFragment(),
-          libsCounter = Object.keys(libs).length;
-
-      while (libsCounter > 0) {
-        var key = Object.keys(libs)[libsCounter-1];
-        if (doc.getElementById(key)) break;
-        var scr = doc.createElement('script');
-        scr.id = 'tvpa';
-        scr.type = 'text/javascript';
-        scr.async = true;
-        scr.src = libs[key].replace(/'/g,'');
-        libsFrag.appendChild(scr);
-        libsCounter--;
-      }
-      ( doc.getElementsByTagName('head')[0]||doc.getElementsByTagName('body')[0] ).appendChild(libsFrag);
-
-      //DEBUG iframe append time
-      root.inlineAppendTime = performance.now();
     }
+
+    widget.run();
+
+    return widget;
   }
 
-  widget.run();
+//Adding the css for host page
+var style = doc.createElement('style'),
+    pre = 'tvp-iframe',
+    holderClass = '.' + pre + '-holder';
 
-  return widget;
+style.innerHTML = holderClass + '{height:0;position:relative;padding-top:56.26%;background-color:black;}'+
+holderClass + '.inline{padding-top:0;}'+
+'.' + pre + '{top:0;left:0;width:100%;height:100%;position:absolute;}';
+appendToHead(style);
+
+if (root.DEBUG) {
+  console.debug("CSS added to host page at: " + (performance.now() - root.DEBUG_start) + " ms");
 }
 
-//Sdd the css for host page
-var style = doc.createElement('style');
-style.innerHTML = '.' + pre + '-holder{height:0;position:relative;padding-top:56.26%;background-color:black;}\
-.' + pre + '{top:0;left:0;width:100%;height:100%;position:absolute;}';
-doc.getElementsByTagName('head')[0].appendChild(style);
-console.timeStamp("CSS for host page appended");
+//Load each widget spots from the page.
+var spots = doc.querySelectorAll('.tvp-sidebar, .tvp-solo'),
+    spotsCount = spots.length;
 
-//We process each of the widget spots from the page...
-var start = function(){
+var load = function(){
   while (spotsCount > 0) {
     var spot = spots[spotsCount - 1]
-    
     Widget(spots[spotsCount - 1]);
-
     spot.remove();
     spotsCount--;
   }
 };
 
-start();
-
-if (root.iframeAppendTime) {
-  console.debug("Iframe append time: " + (iframeAppendTime - startTime) + " ms");
-}
-
-if (root.inlineAppendTime) {
-  console.debug("Inline append time: " + (inlineAppendTime - startTime) + " ms");
-}
+load();
 
 }(window,document));
---
+++ static/player.js
@@ -0,0 +1,160 @@
+//The solo js library.
+;(function(root,doc, parentGlob, parentDoc) {
+
+  //The player singleton. We basically create an instance from the tvpage
+  //player and expose most utilities, helping to encapsualte what is
+  //required for a few players to co-exist.
+  function Player(el, options) {
+    var isset = function(o,p){
+      var val = o;
+      if (p) val = o[p];
+      return 'undefined' !== typeof val;
+    },
+    isEmpty = function(obj) {
+      for(var key in obj) { if (obj.hasOwnProperty(key)) return false;}
+      return true;
+    },
+    debounce = function(func,wait,immediate) {
+      var timeout;  
+      return function() {
+        var context = this, args = arguments;
+        var later = function() {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+      };
+    };
+
+    if (!el) return console.log('need element');
+    if (!isset(options) || !isset(options.data) || options.data.length <= 0) return console.log('need aseets');
+
+    this.el = 'string' === typeof el ? doc.getElementById(el) : el;
+    this.instance = null;
+    this.willCue = null;
+
+    var that = this;
+    this.assets = (function(data){
+      var assets = [],
+          counter = data.length;
+
+      while (counter > 0) {
+        var video = data[counter-1];
+        if (isEmpty(video)) return console.log('empty data');
+        var asset = video.asset;
+        asset.analyticsObj = { vd: video.id, li: video.loginId, pg: video.parentId ? video.parentId : 0 };
+        if (!asset.sources) asset.sources = [{ file: asset.videoId }];
+        asset.type = asset.type || 'youtube';
+        assets.push(asset);
+        counter--;
+      }
+
+      return assets;
+    }(options.data));
+    
+
+    //Methods
+    this.hideEl = function(el){
+      return el.style.display = 'none';
+    };
+    this.showPlayBtn = function(imgUrl){
+      var frag = document.createDocumentFragment(),
+          div = doc.createElement('div');
+
+      div.classList.add('tvp-mp4-poster');
+      div.style.backgroundImage = 'url("'+imgUrl+'")';
+      frag.appendChild(div);
+
+      var btn = this.el.parentNode.getElementsByClassName('tvp-play')[0];
+      btn.style.display = 'block';
+      btn.onclick = function(){
+        if (!that.instance) return;
+        that.hideEl(div);
+        that.hideEl(btn);
+        that.instance.play();
+      };
+      frag.appendChild(btn);
+      this.el.appendChild(frag);
+    };
+    this.play = function(asset){
+      if (!asset) return console.log('need asset');
+      //if ((/Mobi/.test(navigator.userAgent)) ) {
+        this.willCue = true;
+        if ('mp4' === asset.type) this.showPlayBtn(asset.thumbnailUrl);
+      //}
+      if (this.willCue) {
+        this.instance.cueVideo(asset)
+      } else {
+       this.instance.loadVideo(asset);
+     }
+    };
+    
+    var checks = 0, 
+        check = function(o, pr){return 'undefined' !== typeof o[pr];};
+    (function libsReady() {
+      setTimeout(function() {
+        if ( !check(root,'TVPage') || !check(root,'_tvpa') ) {
+          (++checks < 20) ? libsReady() : console.log('limit reached');
+        } else {
+
+          _tvpa.push(['config', {li: options.loginid,
+            gaDomain: 'www.tvpage.tv',
+            logUrl: '\/\/api.tvpage.com\/v1\/__tvpa.gif'
+          }]);
+          _tvpa.push(['track', 'ci', {li:options.loginid}]);
+
+          //We create insntances on the tvpage player.
+          new TVPage.player({
+            poster: true,
+            techOrder: 'html5,flash',
+            analytics: { tvpa: options.tvpa || false },
+            apiBaseUrl: '//app.tvpage.com',
+            swf: '//d2kmhr1caomykv.cloudfront.net/player/assets/tvp/tvp-'+options.version+'-flash.swf',
+            onReady: function(e, pl){
+              that.instance = pl;
+              that.el.querySelector('.tvp-progress-bar').style.backgroundColor = options.progresscolor;
+              
+              var resize = debounce(function() {
+                that.instance.resize(that.el.clientWidth, that.el.clientHeight);
+              }, 180);
+              resize();
+              root.addEventListener('resize', resize);
+            
+              that.current = 0;
+              
+              if (root.DEBUG || parentGlob.DEBUG) {
+                var start = root.DEBUG_start || parentGlob.DEBUG_start;
+                if (start) console.debug("Interaction ready time: " + (performance.now() - start) + " ms");
+              }
+
+              that.play(that.assets[that.current]);
+            },
+            onStateChange: function(e){
+              if ('tvp:media:videoended' === e){
+                that.current++;
+                if (!that.assets[that.current]) {
+                  that.current = 0;
+                }
+                
+                that.play(that.assets[that.current]);
+              }
+            },
+            divId: that.el.id,
+            controls: {
+              active: true,
+              floater: { removeControls: options.removecontrols,transcript: options.transcript}
+            }
+          });
+
+        }
+      },300);
+    })();
+    
+  }
+
+  root.Player = Player;
+
+}(window, document, parent, parent.document));
