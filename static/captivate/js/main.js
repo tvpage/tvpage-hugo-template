4480218847f9dcdce561a502edc8360f922ec258
luisvelazquex <lvelazquez@tvpage.com>
2017-02-02 22:52:22 -0800

    fixing iscroll issue for android

--- static/captivate/js/main.js
+++ static/captivate/js/main.js
@@ -171,7 +171,10 @@
                     scrollX: false,
                     click: true,
                     mouseWheel: true,
-                    scrollbars: true
+                    scrollbars: true,
+                    disablePointer: true, // important to disable the pointer events that causes the issues
+                    disableTouch: false, // false if you want the slider to be usable with touch devices
+                    disableMouse: false // false if you want the slider to be usable with a mouse (desktop)
                 });
             }
             return scroller;
