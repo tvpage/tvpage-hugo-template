bfd8d8164ecac24cc13a4985fa936b68aacf067d
eamurao <eamurao@tvpage.com>
2017-03-20 21:42:44 -0700

    Minify loader.js Update grunt file to include minification of loader.js

--- static/inline/gruntfile.js
+++ static/inline/gruntfile.js
@@ -73,7 +73,8 @@ module.exports = function(grunt) {
               ,'js/libs/utils.js'
               ,'js/libs/player.js'
               , 'js/inline.js'
-              , 'js/index.js']
+              , 'js/index.js'],
+            'loader.min.js' : ['loader.js']
           }
         }
       },
