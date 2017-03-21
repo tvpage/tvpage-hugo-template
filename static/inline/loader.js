ae8b6434cb5911f5aa2043c684deceb1c2e71e99
eamurao <eamurao@tvpage.com>
2017-03-20 21:00:55 -0700

    Use inline/loader.js for dynamic page Dynamic Page product fix

--- static/inline/loader.js
+++ static/inline/loader.js
@@ -93,135 +93,7 @@
         spot.parentNode.removeChild(spot);
 
         self.static = self.domain + '/' + self.type;
-        self.paths = {
-            solo: {
-                dev: [
-                    '//a.tvpage.com/tvpa.min.js',
-                    playerLib,
-                    self.static + '/js/libs/analytics.js',
-                    self.static + '/js/libs/player.js',
-                    self.static + '/js/index.js'
-                ],
-                prod: [
-                    '//a.tvpage.com/tvpa.min.js',
-                    playerLib,
-                    self.static + '/dist/js/scripts.min.js'
-                ]
-            },
-            "solo-cta": {
-                player: {
-                    dev: [
-                        '//a.tvpage.com/tvpa.min.js',
-                        playerLib,
-                        self.static + '/js/libs/analytics.js',
-                        self.static + '/js/libs/player.js',
-                        self.static + '/js/index.js'
-                    ],
-                    prod: [
-                        '//a.tvpage.com/tvpa.min.js',
-                        playerLib,
-                        self.static + '/dist/js/scripts.min.js'
-                    ]
-                },
-                modal: {
-                    dev: [
-                        '//a.tvpage.com/tvpa.min.js',
-                        playerLib,
-                        (isMobile ? self.static + '/js/vendor/jquery.js' : ''),
-                        self.static + '/js/libs/utils.js',
-                        self.static + '/js/libs/analytics.js',
-                        self.static + '/js/libs/player.js',
-                        self.static + '/js/' + mobilePath + 'modal/index.js'
-                    ],
-                    prod: [
-                        '//a.tvpage.com/tvpa.min.js',
-                        playerLib,
-                        (isMobile ? self.static + '/js/vendor/jquery.js' : ''),
-                        self.static + '/dist/js/' + mobilePath + 'modal/scripts.min.js'
-                    ]
-                }
-            },
-            sidebar: {
-                gallery: {
-                    dev: [
-                        self.static + '/js/libs/utils.js',
-                        self.static + '/js/grid.js',
-                        self.static + '/js/index.js'
-                    ],
-                    prod: [
-                        self.static + '/dist/js/scripts.min.js'
-                    ]
-                },
-                modal: {
-                    dev: [
-                        '//a.tvpage.com/tvpa.min.js',
-                        playerLib,
-                        (isMobile ? self.static + '/js/vendor/jquery.js' : ''),
-                        (isMobile ? '' : self.static + '/js/vendor/simple-scrollbar.min.js'),
-                        self.static + '/js/libs/utils.js',
-                        self.static + '/js/libs/analytics.js',
-                        self.static + '/js/libs/player.js',
-                        self.static + '/js/' + mobilePath + 'modal/index.js'
-                    ],
-                    prod: [
-                        '//a.tvpage.com/tvpa.min.js',
-                        playerLib,
-                        self.static + '/dist/js/' + mobilePath + 'modal/scripts.min.js'
-                    ]
-                }
-            },
-            carousel: {
-                gallery: {
-                    dev: [
-                        self.static + '/js/vendor/jquery.js',
-                        self.static + '/js/libs/utils.js',
-                        self.static + '/js/carousel.js',
-                        self.static + '/js/index.js'
-                    ],
-                    prod: [
-                        self.static + '/dist/js/scripts.min.js'
-                    ]
-                },
-                modal: {
-                    dev: [
-                        '//a.tvpage.com/tvpa.min.js',
-                        playerLib,
-                        (isMobile ? self.static + '/js/vendor/jquery.js' : ''),
-                        (isMobile ? '' : self.static + '/js/vendor/simple-scrollbar.min.js'),
-                        self.static + '/js/libs/utils.js',
-                        self.static + '/js/libs/analytics.js',
-                        self.static + '/js/libs/player.js',
-                        self.static + '/js/' + mobilePath + 'modal/index.js'
-                    ],
-                    prod: [
-                        '//a.tvpage.com/tvpa.min.js',
-                        playerLib,
-                        self.static + '/dist/js/' + mobilePath + 'modal/scripts.min.js'
-                    ]
-                }
-            },
-            inline : {
-                dev: [
-                    '//a.tvpage.com/tvpa.min.js',
-                    playerLib,
-                    self.static + '/js/vendor/jquery.js',
-                    self.static + '/js/libs/analytics.js',
-                    self.static + '/js/vendor/slick-min.js',
-                    self.static + '/js/vendor/simple-scrollbar.min.js',
-                    self.static + '/js/libs/utils.js',
-                    self.static + '/js/libs/player.js',
-                    self.static + '/js/index.js',
-                    self.static + '/js/inline.js'                    
-                ],
-                prod: [
-                    '//a.tvpage.com/tvpa.min.js',
-                    playerLib,
-                    self.static + '/dist/js/scripts.min.js'
-                ]
-            }
-        };
 
-        self.paths['solo-click'] = self.paths.solo;
         self.config = {};
 
         if (isset(window, '__TVPage__') && isset(__TVPage__, 'config') && isset(__TVPage__.config, self.id)) {
@@ -241,12 +113,6 @@
 
             var iframe = self.holder.querySelector('iframe');
 
-            window.addEventListener('resize', debounce(function() {
-                window.postMessage({
-                    event: self.senderId + ':holder_resize',
-                    size: [self.holder.offsetWidth, self.holder.offsetHeight]
-                }, '*');
-            }, 50));
 
             //Central point for cross-domain messaging between iframes, we always us the host page window.
             window.addEventListener('message', function(e) {  
@@ -255,151 +121,8 @@
                 var data = e.data;
                 var id = self.id;
 
-                self.iframeModalId = 'tvp-iframe-modal-' + id;
-
                 if (self.senderId + ':render' === eventName || self.senderId + ':resize' === eventName) {
                     self.holder.style.height = e.data.height;
-                }
-                
-                if (self.senderId + ':video_click' === eventName) {
-                    var runTime = (data.runTime || (isset(window, '__TVPage__') ? __TVPage__ : {}) ).config[id];
-                    var selectedVideo = data.selectedVideo || {};
-
-                    self.data[id] = self[id] || {};
-                    self[id] = {
-                        data: data.videos || [],
-                        selectedVideo: selectedVideo,
-                        runTime: runTime
-                    };
-
-                    var modalFrag = document.createDocumentFragment();
-                    var overlay = document.createElement('div');
-                    overlay.classList.add('tvp-modal-overlay');
-                    modalFrag.appendChild(overlay);
-                    var modal = document.createElement('div');
-                    modal.classList.add('tvp-modal');
-                    modal.innerHTML = '<div class="tvp-modal-wrapper"><div class="tvp-modal-content"><div class="tvp-modal-header">' +
-                        '<svg class="tvp-modal-close" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">' +
-                        '<path fill="#ffffff" d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/><path d="M0 0h24v24H0z" fill="none"/></svg>' +
-                        '<h4 class="tvp-modal-title">' + selectedVideo.title + '</h4></div><div class="tvp-modal-body"><div class="tvp-modal-iframe-holder"><iframe id="' + self.iframeModalId + '" src="about:blank"' +
-                        'allowfullscreen frameborder="0" scrolling="no" class="tvp-iframe-modal"></iframe></div></div></div></div>';
-
-                    modalFrag.appendChild(modal);
-
-                    var button = modalFrag.querySelector('.tvp-modal-close');
-                    var close = function() {
-                        document.body.classList.remove('tvp-modal-open');
-                        button.removeEventListener('click', close, false);
-                        [modal, overlay, button].forEach(function(el) {
-                            el.parentNode.removeChild(el);
-                        });
-                    };
-                    button.addEventListener('click', close);
-
-                    var iframeModal = modalFrag.querySelector('.tvp-iframe-modal');
-
-                    var body = document.body;
-
-                    body.classList.add('tvp-modal-open');
-                    body.appendChild(modalFrag);
-
-                    var iframeModalDoc = iframeModal.contentWindow.document;
-
-                    iframeModalDoc.open().write(getIframeHtml({
-                        domain: self.domain,
-                        id: id,
-                        html: function () {
-                            var html = '<div id="' + id + '" class="tvp-clearfix iframe-content">';
-
-                            if (isMobile) {
-                                html += '<div class="tvp-player"><div id="tvp-player-el"></div></div><div class="tvp-products"><div class="tvp-products-carousel"></div></div>';
-                            } else {
-                                html += '<div class="tvp-player-holder"><div class="tvp-player"><div id="tvp-player-el"></div></div></div>';
-
-                                if ("solo-cta" !== self.type) {
-                                    html += '<div class="tvp-products-holder"><div class="tvp-products"></div></div>';
-                                }
-                            }
-
-                            return (html + '</div>');
-                        },
-                        js: self.paths[self.type].modal[env].filter(Boolean),
-                        css: function () {
-                            var files = [self.static + (window.DEBUG ? '/' : '/dist/') + 'css/' + mobilePath + 'modal/styles' + cssExt];
-                            if (window.DEBUG) {
-                                if (isMobile) {
-                                    files.push(self.static + '/css/vendor/slick.css');
-                                } else {
-                                    files.push(self.static + '/css/vendor/simple-scrollbar.css');
-                                }
-                            }
-                            return files;
-                        }
-                    }));
-                    iframeModalDoc.close();
-                }
-
-                if (self.senderId + ':modal_initialized' === eventName) {
-                    var widgetData = self[id];
-                    var iframeModal = document.getElementById(self.iframeModalId);
-
-                    if (iframeModal.contentWindow) {
-                        iframeModal.contentWindow.postMessage({
-                            event: self.senderId + ':modal_data',
-                            data: widgetData.data,
-                            selectedVideo: widgetData.selectedVideo,
-                            runTime: widgetData.runTime
-                        }, '*');
-                    }
-
-                    if (/iPad|iPhone|iPod|iPhone Simulator|iPad Simulator/.test(navigator.userAgent) && !window.MSStream) {
-                        var onOrientationChange = debounce(function () {
-                            var iframeModal = document.getElementById(self.iframeModalId);
-                            if (iframeModal && iframeModal.contentWindow) {
-                                var widthRef = iframeModal.parentNode.offsetWidth;
-                                iframeModal.contentWindow.window.postMessage({
-                                    event: self.senderId + ':modal_holder_resize',
-                                    size: [widthRef, Math.floor(widthRef * (9 / 16))]
-                                },'*');
-                            }
-                        },30);
-                        var orientationChangeEvent = 'onorientationchange' in window ? 'orientationchange' : 'resize';
-                        window.removeEventListener(orientationChangeEvent,onOrientationChange, false);
-                        window.addEventListener(orientationChangeEvent,onOrientationChange, false);
-                    }
-                }
-
-                if (self.senderId + ':modal_resized' === eventName) {
-                    document.getElementById(self.iframeModalId).style.height = e.data.height;
-                }
-
-                if (self.senderId + ':player_next' === eventName) {
-                    document.querySelector('.tvp-modal-title').innerHTML = e.data.next.assetTitle;
-                }
-
-                var iframeModalHolder = document.querySelector('.tvp-modal-iframe-holder');
-
-                if (self.senderId + ':modal_no_products' === eventName) {
-                    if (!isMobile) {
-                        var pLabel = document.querySelector('.tvp-products-headline');
-                        if (pLabel) {
-                            pLabel.remove();
-                        }
-                    }
-
-                    iframeModalHolder.classList.remove('products');
-                    iframeModalHolder.classList.add('no-products');
-                }
-
-                if (self.senderId + ':modal_products' === eventName) {
-                    if (!isMobile) {
-                        var productsLabel = document.createElement('p');
-                        productsLabel.classList.add('tvp-products-headline');
-                        productsLabel.innerHTML = 'Related Products';
-                        document.querySelector('.tvp-modal-header').appendChild(productsLabel);
-                    }
-                    iframeModalHolder.classList.remove('no-products');
-                    iframeModalHolder.classList.add('products');
                 }
             });
 
@@ -411,21 +134,40 @@
                 var iframeDoc = iframe.contentWindow.document;
 
                 iframeDoc.open().write(getIframeHtml({
-                    js: function () {                        
-                        var jsFiles = self.paths[self.type];
-                        if ('sidebar' === self.type || 'carousel' === self.type) {
-                            jsFiles = jsFiles.gallery;
-                        } else if ('solo-cta' === self.type) {
-                            jsFiles = jsFiles.player;
-                        }                        
-                        return jsFiles[env];
+                    js: function () {
+                        if (window.DEBUG) {
+                            return [
+                                '//a.tvpage.com/tvpa.min.js',
+                                playerLib,
+                                self.static + '/js/vendor/jquery.js',
+                                self.static + '/js/libs/analytics.js',
+                                self.static + '/js/vendor/slick-min.js',
+                                self.static + '/js/vendor/simple-scrollbar.min.js',
+                                self.static + '/js/libs/utils.js',
+                                self.static + '/js/libs/player.js',
+                                self.static + '/js/index.js',
+                                self.static + '/js/inline.js'                              
+                            ]
+                        }
+                        else{
+                            return [
+                                '//a.tvpage.com/tvpa.min.js',
+                                playerLib,
+                                self.static + '/dist/js/scripts.min.js'
+                            ]
+                        }
                     }(),
                     css: function () {
-                        var cssFiles = [self.static + (!window.DEBUG ? '/' : '/dist/') + 'css/styles' + cssExt]
-                        if ('carousel' === self.type && window.DEBUG) {
-                            cssFiles = cssFiles.concat([self.static + '/css/vendor/slick.css']);
+                        if (!window.DEBUG) {
+                            return [
+                                self.static + '/css/styles.css'
+                            ]
                         }
-                        return cssFiles;
+                        else{
+                            return [
+                                self.static + '/dist/css/styles.min.css'
+                            ]
+                        }
                     },
                     className: self.dataMethod,
                     domain: self.domain,
@@ -448,7 +190,7 @@
     }
 
     function load () {
-        var spots = document.querySelectorAll('.tvp-sidebar, .tvp-carousel, .tvp-solo, .tvp-solo-cta, .tvp-solo-append, .tvp-inline');
+        var spots = document.querySelectorAll('.tvp-inline');
         for (var i = 0; i < spots.length; i++) {            
             var widget  = Widget(spots[i]);
             widget.initialize();
