af7a75e963b12dfaab8acf36d3cbbcda893db7bb
eamurao <eamurao@tvpage.com>
2017-03-29 00:48:19 -0700

    Remove unused code

--- static/inline/js/index.js
+++ static/inline/js/index.js
@@ -1,64 +1,4 @@
 ;(function(window,document) {
-
-    var isset = function(o,p){
-        var val = o;
-        if (p) val = o[p];
-        return 'undefined' !== typeof val;
-    };
-
-    var jsonpCall = function(opts,callback){
-        var s = document.createElement('script');
-        s.src = opts.src;
-        if (!callback || 'function' !== typeof callback) return;
-        window[opts.cbName || 'callback'] = callback;
-        var b = opts.body || document.body;
-        b.appendChild(s);
-    };
-
-    var extend = function(out) {
-        out = out || {};
-        for (var i = 1; i < arguments.length; i++) {
-            if (!arguments[i])
-                continue;
-
-            for (var key in arguments[i]) {
-                if (arguments[i].hasOwnProperty(key))
-                    out[key] = arguments[i][key];
-            }
-        }
-        return out;
-    };
-
-    var getSettings = function(type){
-        var getConfig = function(g){
-            var c = {};
-            if (isset(g) && isset(g,'__TVPage__') && isset(g.__TVPage__, 'config')) {
-                c = g.__TVPage__.config;
-            }
-            return c;
-        };
-        var s = {};
-        if ('dynamic' === type) {
-            var config = getConfig(parent);
-            var id = document.body.getAttribute('data-id');
-            if (!isset(config, id)) return;
-            s = config[id];
-            s.name = id;
-            s.domain = document.body.getAttribute('data-domain') || '';
-        } else if ('inline' === type && type && type.length) {
-            var config = getConfig(parent);
-            s = config[type];
-            s.name = type;
-        } else if ('static' === type) {
-            var config = getConfig(window);
-            var id = document.body.getAttribute('data-id');
-            if (!isset(config, id)) return;
-            s = config[id];
-            s.name = id;
-        }
-        return s;
-    };
-
     var render = function(target,data){
         if (!target) return;
         var frag = document.createDocumentFragment();
@@ -76,57 +16,25 @@
     var body = document.body;
 
     var initialize = function(){        
-        if (body.classList.contains('dynamic')) {
-            (function(settings){
-                var inlineSettings = JSON.parse(JSON.stringify(settings));
-                var name = settings.name;
-                
-                render(body,{
-                    id: name,
-                    title: settings.title || 'Recommended Videos',
-                    inlineTemplate: settings.templates.inline
-                });
+        var settings = {};
+        var body = document.body;
 
-                jsonpCall({
-                    src: settings.domain + '/inline/options.js',
-                    cbName: 'tvpcallback'
-                },function(data){
-                    if (!data) return;
-                    var options = data.option;
-                    var opts = {};
-
-                    for (var key in options) {
-                        var option = options[key];
-                        opts[option.code] = option.value;
-                    }                    
-                    inlineSettings = extend(inlineSettings, opts);
-                    $.when(
-                        $.getScript('//a.tvpage.com/tvpa.min.js'),
-                        $.getScript('https://cdnjs.tvpage.com/tvplayer/tvp-'+opts.player_version+'.min.js')
-                    ).done(function (a, b) {
-                        Inline(name, inlineSettings);
-                    });                    
-                });
-
-            }(getSettings('dynamic')));
-        } else {
-            (function(settings){                
-                var inlineSettings = JSON.parse(JSON.stringify(settings));
-                var name = settings.name;
-
-                if(Utils.isMobile) {
-                    document.getElementById(name).classList.add('mobile');
-                }
-
-                $.when(
-                    $.getScript('//a.tvpage.com/tvpa.min.js'),
-                    $.getScript('https://cdnjs.tvpage.com/tvplayer/tvp-'+inlineSettings.player_version+'.min.js')
-                ).done(function (a, b) {
-                    Inline(name, inlineSettings);
-                });
-
-            }(getSettings('static')));
+        if (Utils.isset(parent) && Utils.isset(parent,'__TVPage__') && Utils.isset(parent.__TVPage__, 'config')) {
+            settings = parent.__TVPage__.config[body.getAttribute('data-id')];
         }
+
+        var inlineSettings = JSON.parse(JSON.stringify(settings));
+        render(body,{
+            id: settings.name,
+            title: settings.title || 'Recommended Videos',
+            inlineTemplate: settings.templates.inline
+        });
+        $.when(
+            $.getScript('//a.tvpage.com/tvpa.min.js'),
+            $.getScript('https://cdnjs.tvpage.com/tvplayer/tvp-'+settings.player_version+'.min.js')
+        ).done(function (a, b) {
+            Inline(settings.name, inlineSettings);
+        });
     };
 
     initialize();
