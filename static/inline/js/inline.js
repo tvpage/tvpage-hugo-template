fda2953944c6b7e83d4fa7d5eb44e4ba25acb788
Ricardo Fornes <ricardo@tvpage.com>
2017-09-14 15:48:42 -0700

    DEV-8732 expose API order options for inline template

--- static/inline/js/inline.js
+++ static/inline/js/inline.js
@@ -19,6 +19,7 @@
     var inlineEl = null;
     var productRatingEmptyIsBordered = false;
     var hasProducts = true;
+    var allOptions = {};
 
     var renderedApproach = function () {
         if (document.body.clientWidth < breakpoint) {
@@ -61,6 +62,7 @@
     var loadProducts = function(videoId, loginId, fn) {
         if (!videoId) return;
         var src = '//api.tvpage.com/v1/videos/' + videoId + '/products?X-login-id=' + loginId;
+        src += '&o=' + allOptions.products_order_by + '&od=' + allOptions.products_order_direction;
         var cbName = 'tvp_' + Math.floor(Math.random() * 555);
         src += '&callback=' + cbName;
         var script = document.createElement('script');
@@ -298,6 +300,7 @@
     };
 
     function Inline(el, options) {
+        allOptions = options;
         currentApproach = renderedApproach();
         xchg = options.xchg || false;
         loginId = (options.loginId || options.loginid) || 0;
@@ -437,6 +440,8 @@
             var getChannelVideos = function(callback){
                 var channel_id = Utils.isEmpty(channel) ? channelId : channel.id;
                 var params = channel.parameters || {};
+                params.o = options.videos_order_by;
+                params.od = options.videos_order_direction;
                 var src = '//api.tvpage.com/v1/channels/' + channel_id + '/videos?X-login-id=' + loginId;
                 for (var p in params) { src += '&' + p + '=' + params[p];}
                 var cbName = options.callbackName || 'tvp_' + Math.floor(Math.random() * 555);
