e0fd15b143fa5eeeed4a074de8b5955930a72927
sdorle <sdorle@tvpage.com>
2017-09-29 17:16:31 -0700

    DEV-8852: slicks dots dont show when products equals 1

--- static/sidebar-single/js/mobile/modal/index.js
+++ static/sidebar-single/js/mobile/modal/index.js
@@ -155,7 +155,7 @@
                 config.centerMode = centerMode;
                 config.centerPadding = centerPadding;
                 
-                if (data.length <= 5) {
+                if (data.length <= 5 && data.length > 1) {
                     var dotsHolderClass = "tvp-slider-dots-holder";
                     var dotsHolderElement = container.querySelector("." + dotsHolderClass);
                     if (dotsHolderElement) {
