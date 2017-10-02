71ef3294e8fe4c78f8ae87dc38824a65ef55428d
Ricardo Fornes <ricardo@tvpage.com>
2017-10-02 12:01:52 -0700

    Adding Coverage

--- static/karma.conf.js
+++ static/karma.conf.js
@@ -9,17 +9,17 @@ module.exports = function(karma) {
       'vendor/tvp-3.1.2.min.js',
       'vendor/tvpa.min.js',
 
-      'libs/**/*.js',
+      'sdk/libs/**/*.js',
 
       'test/**/*Spec.js',
       'test/**/*.html',
       'test/**/*.json'
     ],
 
-    reporters: [ 'progress', 'coverage' ],
+    reporters: [ 'coverage' ],
 
     preprocessors: {
-      'libs/**/*.js': 'coverage',
+      'sdk/libs/**/*.js': 'coverage',      
       'test/**/*.html' : 'html2js',
       'test/**/*.json' : 'json_fixtures'
     },
@@ -34,21 +34,10 @@ module.exports = function(karma) {
 
     autoWatch: false,
 
-    coverageReporter:{
-      dir: 'coverage/',
+    coverageReporter: {
       reporters: [
-        {
-          type: 'text-summary'
-        },
-        {
-          subdir: 'html',
-          type : 'html'
-        },
-        {
-          subdir: 'cobertura',
-          file: 'cobertura.xml',
-          type : 'cobertura'
-        }
+          {type: 'text-summary'},
+          {type: 'clover', dir: 'coverage', subdir: '.', file: 'clover.xml'}
       ]
     },
 
