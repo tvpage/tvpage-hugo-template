9a1c765668e34d632d85976e44772fe7d7a1a3c8
Ricardo Fornes <ricardo@tvpage.com>
2017-10-02 15:55:51 -0700

    Adding Coverage

---
+++ static/check.js
@@ -0,0 +1,21 @@
+var path = require("path");
+var fs = require("fs");
+
+fs.readdir('../artifacts', function(err, items) {
+  if(err)
+    console.log(err)
+});
+
+var branch   = process.env.SCRUTINIZER_BRANCH;
+var prNumber   = process.env.SCRUTINIZER_PR_NUMBER;
+var srcBranch   = process.env.SCRUTINIZER_PR_SOURCE_BRANCH;
+var sha   = process.env.SCRUTINIZER_SHA1;
+var uuid   = process.env.SCRUTINIZER_INSPECTION_UUID;
+var project   = process.env.SCRUTINIZER_PROJECT;
+
+console.log( branch )
+console.log( prNumber )
+console.log( srcBranch )
+console.log( sha )
+console.log( uuid )
+console.log( project )
