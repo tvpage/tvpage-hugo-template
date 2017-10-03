d092fc1af6ee78337358352223f07d20e54a13b6
Ricardo Fornes <ricardo@tvpage.com>
2017-10-02 17:45:49 -0700

    Adding Coverage

--- static/check.js
+++ static/check.js
@@ -1,40 +1,9 @@
-var fs = require("fs");
+//var request = require('request');
 
-fs.readdir(__dirname + '/../../artifacts', function(err, items) {
+var UUID = process.env.SCRUTINIZER_INSPECTION_UUID;
+var PROJECT = process.env.SCRUTINIZER_PROJECT;
+var GITHUB_USERNAME = process.env.GITHUB_USERNAME;
+var GITHUB_ACCESS_TOKEN = process.env.GITHUB_ACCESS_TOKEN;
 
-  console.log(err)
-  console.log(items)
-
-  if(err) {
-    return console.log(err);
-  }
-  
-  for (var i=0; i<items.length; i++) {
-    
-    console.log(items[i])
-    
-    // if (fs.existsSync(filePath)) {
-    //     var newFilePath = replaceExt(filePath, '.js');
-    //     console.log("replacing ", filePath);
-    //     fs.rename(filePath, newFilePath, function(err) {
-    //         if (err) console.log('Something went bad ' + err);
-    //     });
-    // }	else {
-    //   console.warn("no html file found ", filePath);
-    // }
-  };
-});
-
-var branch   = process.env.SCRUTINIZER_BRANCH;
-var prNumber   = process.env.SCRUTINIZER_PR_NUMBER;
-var srcBranch   = process.env.SCRUTINIZER_PR_SOURCE_BRANCH;
-var sha   = process.env.SCRUTINIZER_SHA1;
-var uuid   = process.env.SCRUTINIZER_INSPECTION_UUID;
-var project   = process.env.SCRUTINIZER_PROJECT;
-
-console.log( branch )
-console.log( prNumber )
-console.log( srcBranch )
-console.log( sha )
-console.log( uuid )
-console.log( project )
+console.log(GITHUB_USERNAME)
+console.log(GITHUB_ACCESS_TOKEN)
