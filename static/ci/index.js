4cc07093a75a3673deca5dec19c64b357c163e77
Ricardo Fornes <ricardo@tvpage.com>
2017-10-06 15:55:42 -0700

    Add ci updates

--- static/ci/index.js
+++ static/ci/index.js
@@ -1,32 +1,41 @@
 var GitHubApi = require('github');
-var github = new GitHubApi();
 var exit = require('exit');
-
-github.authenticate({
-  type: "basic",
-  username: 'rfornes',
-  password: 'Jrfp030685311'
-});
-
 var fs = require('fs');
 var xml2js = require('xml2js')
 var parser = new xml2js.Parser();
+var colors = require('colors/safe');
 
-var filterExcludes = function(files){
-  var excludes = [
-    'static/karma.conf.js',
-    'static/package.json',
-    'static/package-lock.json',
-    'static/test'
-  ];
+console.log(process.env.repo)
+console.log(process.env.excludes)
+console.log(process.env.includes)
+
+exit(1);
+
+var fail = function(m,e){
+  console.log(colors.red(m),e || '');
+  exit(1);
+};
+
+var pass = function(){
+  console.log(colors.green("check passed"));
+  exit(0);
+};
+
+//Removes the file paths that we don't need to run the check against. This
+//is based in the includes/excludes options.
+function filter(files){
+  var includes = process.env.includes;
+  var excludes = process.env.excludes;
 
   return files.filter(function(file){
-    var name = file.filename;
-    if(-1 === name.indexOf('static/sdk'))
-      return false;
+    var path = file.filename;
+
+    for (var i = 0; i < includes.length; i++)
+      if(-1 === path.indexOf(includes[i]))
+        return false;
 
     for (var j = 0; j < excludes.length; j++)
-      if(-1 !== name.indexOf(excludes[j]))
+      if(-1 !== path.indexOf(excludes[j]))
         return false;
 
     return true;
@@ -51,94 +60,102 @@ var checkCoverageFileMetrics = function(file){
   return true;
 };
 
-var searchCoverageFile = function(coveredFiles,query){
-  for (var j = 0; j < coveredFiles.length; j++) {
-    var coveredFile = coveredFiles[j];
-    if(-1 !== coveredFile.path.indexOf(query))
-      return coveredFile;
-  }
-  return null;
-};
-
-var getCoverageFiles = function(res){
-  var coverage = res.coverage;
-  var project = {};
-
-  if(coverage.project && coverage.project.length)
-    project = coverage.project[0];
-  
-  var files = [];
-  var package = project.package[0];
-
-  if(package && package.file)
-    files = package.file;
-
-  return files.map(function(f){
-    return {
-      path: f.$.path,
-      metrics: f.metrics[0].$
-    }
-  });
-};
-
-function whenFilesLoaded(files){
-  files = filterExcludes(files);
-
-  fs.readFile(__dirname + '/../coverage/clover.xml', function(err, data) {
-    if(err){
-      throw new Error("can't read coverage report");
+var getCoverageFiles = function(cback){
+  fs.readFile(__dirname + '/../coverage/clover.xml', function(error, data) {
+    if(error){
+      fail("Can't read coverage report.\n", error);
     }
 
-    parser.parseString(data, function (err, result) {
-      if(err){
-        throw new Error("can't parse xml");
-      }
+    parser.parseString(data, function (error, result) {
+      if(error){
+        fail("Can't parse xml.\n", error);
+      } else if('function' === typeof cback){
 
-      for (var i = 0; i < files.length; i++) {
-        var coverageFile = searchCoverageFile(
-          getCoverageFiles(result),
-          files[i].filename
-        );
+        var coverage = result.coverage;
+        var project = {};
+      
+        if(coverage.project && coverage.project.length)
+          project = coverage.project[0];
         
-        if(!coverageFile){
-          console.error("The file doesn't have coverage data. \n");
-          exit(1);
-        } else if(!checkCoverageFileMetrics(coverageFile)){
-          console.error("File's coverage is under 75%. \n");
-          exit(1);
-        }
-      }
+        var files = [];
+        var package = project.package[0];
+      
+        if(package && package.file)
+          files = package.file;
+      
+        files = files.map(function(f){
+          return {
+            path: f.$.path,
+            metrics: f.metrics[0].$
+          };
+        });
 
-      exit(0);
+        cback(files);
+      }
     });
   });
 };
 
-var perPage = 10;
-var page = 0;
-var allFiles = [];
+function checkFiles(files){
+  getCoverageFiles(function(coverageFiles){
+    files = filter(files);
+    
+    for (var i = 0; i < files.length; i++) {
+      var path = files[i].filename;
+      var matchFile = null;
 
-(function loadFiles() {
-  github.pullRequests.getFiles({
-    owner: 'tvpage',
-    repo: 'tvpage-hugo-template',
-    number: process.env.SCRUTINIZER_PR_NUMBER || 84,
-    per_page: perPage,
-    page: page
-  }, function(e, res) {
-    if (e) {
-      throw new Error("can't get repo files");
+      for (var j = 0; j < coverageFiles.length; j++) {
+        var coverageFile = coverageFiles[j];
+        if(-1 !== coverageFile.path.indexOf(path))
+          matchFile = coverageFile;
+      }
+      
+      if(matchFile && checkCoverageFileMetrics(matchFile)){
+        pass();
+      }else{
+        fail("File: "+ path +" coverage is less than 75 or null.\n");
+      }
     }
 
-    var pageFiles = res.data || [];
+    pass();
+  });
+};
 
-    allFiles = allFiles.concat(pageFiles);
+var filesPerPage = 10;
+var filesPage = 0;
+var pullRequestFiles = [];
+
+(function loadPullRequestFiles() {
+  var github = new GitHubApi();
+  var repo = process.env.repo;
+  
+  github.authenticate({
+    type: "basic",
+    username: 'rfornes',
+    password: 'Jrfp030685311'
+  });
+
+  github.pullRequests.getFiles({
+    owner: repo.owner,
+    repo: repo.name,
+    number: process.env.SCRUTINIZER_PR_NUMBER || 78,
+    per_page: filesPerPage,
+    page: filesPage
+  }, function(error, response) {
+    if (error) {
+      fail("Can't get repo files.\n", error);
+    } 
+
+    var responseFiles = response.data || [];
+
+    pullRequestFiles = pullRequestFiles.concat(responseFiles);
     
-    if(pageFiles.length === perPage){
-      ++page;
-      loadFiles();
+    if(responseFiles.length === filesPerPage){
+      ++filesPage;
+      loadPullRequestFiles();
     }else{
-      whenFilesLoaded(allFiles);
+      console.log(colors.yellow("Pull request with: "+ pullRequestFiles.length +" files.\n"));
+      checkFiles(pullRequestFiles);
     }
   });
 }())
