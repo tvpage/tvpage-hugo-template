269f9e536774975208cd4720895341afd6b81136
sdorle <sdorle@tvpage.com>
2017-11-10 15:38:50 -0800

    local 3.1.4 player

--- static/carousel/index.js
+++ static/carousel/index.js
@@ -273,7 +273,8 @@ function handleVideoClick(e){
     iframeModalDocument = iframeModal.contentWindow.document;
     
     //Some logic to include the player library.. we support diff things.
-    var playerUrl = "https://cdnjs.tvpage.com/tvplayer/tvp-" + config.player_version + ".min.js";
+    // var playerUrl = "https://cdnjs.tvpage.com/tvplayer/tvp-" + config.player_version + ".min.js";
+    var playerUrl = "https://cdnjs.tvpage.com/tvplayer/local/tvp-3.1.4.min.js";
     if (config.player_url && (config.player_url + "").trim().length) {
         playerUrl = config.player_url;
     }
