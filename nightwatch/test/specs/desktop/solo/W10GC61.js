fa9fc752a11e30ee9a11d952aa2cf25af2b0f6fd
sdorle <sdorle@tvpage.com>
2017-11-21 18:23:41 -0800

    solo update

--- nightwatch/test/specs/desktop/solo/W10GC61.js
+++ nightwatch/test/specs/desktop/solo/W10GC61.js
@@ -6,6 +6,11 @@ var DATA = {
   SLA: 10000
 };
 
+var aCounts = {
+  'ci': 0,
+  'vv': 0
+};
+
 var guiTest = {
   init: function (client, frameId) {
     client
@@ -30,104 +35,86 @@ var guiTest = {
 
     return decodeURIComponent(results[2].replace(/\+/g, " "));
   },
+  inArray : function(needle, haystack, isIndex) {
+    var found = false;
+
+    haystack.forEach(function(value, i) {
+      if (needle == value) {
+        found = (isIndex === true ? i : true);
+      }
+    });
+
+    return found;
+  },
   analytics: function(client, frame, events, aData) {
-      var counts = {
-        'ci': 0,
-        'vv': 0,
-        'pi': 0,
-        'pk': 0
-      };
-  
-      var tests = {
-        'ci': function(client, src) {
-          console.log(">>> Checking CI <<<");
+    var tests = {
+      'ci': function(client, src, current) {
+        console.log(">>> Checking CI <<<");
 
-          client.waitForElementVisible('p#analtyticsTestCI', 6000);
-          client.expect.element('p#analtyticsTestCI').to.be.present;
-          this.assert.equal(counts['ci'], 1);
-          var li = guiTest.getParameterByName('li', src);
-          this.assert.equal(li, aData.LOGIN_ID);
-          var url = guiTest.getParameterByName('url', src);
-          this.assert.equal(url, DATA.URL);
-          // TODO: Need to check CID
-          //var cid = getParameterByName('cid', src);
-          //this.assert.ok(cid);
-        },
-        'vv': function(client, src) {
-          console.log(">>> Checking VV <<<");
+        client.waitForElementVisible('p#analtyticsTestCI', 6000);
+        client.expect.element('p#analtyticsTestCI').to.be.present;
+        this.assert.equal(aCounts['ci'], 1);
 
-          client.waitForElementVisible('p#analtyticsTestVV', 6000);
-          client.expect.element('p#analtyticsTestVV').to.be.present;
-          var li = guiTest.getParameterByName('li', src);
-          this.assert.equal(li, aData.LOGIN_ID);
-          var url = guiTest.getParameterByName('url', src);
-          this.assert.equal(url, DATA.URL);
-          var pg = guiTest.getParameterByName('pg', src);
-          this.assert.equal(pg, aData.CHANNEL_ID);
-          var vd = guiTest.getParameterByName('vd', src);
-          this.assert.equal(vd, aData.VIDEO_ID);
-          var vvs = guiTest.getParameterByName('vvs', src);
-          this.assert.ok(vvs);
-          DATA.vvs = vvs;
-        },
-        'pi': function(client, src) {
-          console.log(">>> Checking PI <<<");
+        var li = guiTest.getParameterByName('li', src);
+        this.assert.equal(li, aData.LOGIN_ID);
 
-          var url = guiTest.getParameterByName('url', src);
-          this.assert.equal(url, DATA.URL);
-          var li = guiTest.getParameterByName('li', src);
-          this.assert.equal(li, aData.LOGIN_ID);
-          var pg = guiTest.getParameterByName('pg', src);
-          this.assert.equal(pg, aData.CHANNEL_ID);
-          var vd = guiTest.getParameterByName('vd', src);
-          this.assert.equal(vd, aData.VIDEO_ID);
-          var vd = guiTest.getParameterByName('vd', src);
-          this.assert.equal(vd, aData.VIDEO_ID);
-          // TODO: Need to check CID
-          //var cid = getParameterByName('cid', src);
-          //this.assert.ok(cid);
-        },
-        'pk': function(client, src) {
-          console.log(">>> Checking PK <<<");
+        var url = guiTest.getParameterByName('url', src);
+        this.assert.equal(url, DATA.URL);
 
-          client.waitForElementVisible('p.analtyticsTestPK', 6000);
-          client.expect.element('p.analtyticsTestPK').to.be.present;
-          var url = guiTest.getParameterByName('url', src);
-          this.assert.equal(url, DATA.URL);
-          var li = guiTest.getParameterByName('li', src);
-          this.assert.equal(li, aData.LOGIN_ID);
-          var pg = guiTest.getParameterByName('pg', src);
-          this.assert.equal(pg, aData.CHANNEL_ID);
-          var vd = guiTest.getParameterByName('vd', src);
-          this.assert.equal(vd, aData.VIDEO_ID);
-          var ct = guiTest.getParameterByName('ct', src);
-          this.assert.equal(ct, aData.product.ID);
-          // TODO: Need to check CID
-          //var cid = getParameterByName('cid', src);
-          //this.assert.ok(cid);
-          client.elements("class name", "analtyticsTestPK", function(result) {
-            this.assert.ok(counts['pk'] <= result.value.length);
-          });
-        }
-      };
+        // var cid = guiTest.getParameterByName('cid', src);
+        // this.assert.ok(cid);
+      },
+      'vv': function(client, src, current) {
+        console.log(">>> Checking VV <<<");
 
-      client.frame(frame).elements("tag name", "script", function(result) {
-        result.value.forEach(function(script) {
-          client.elementIdAttribute(script.ELEMENT, 'src', function(res) {
-            var src = res.value,
-                THAT = this;
+        var li = guiTest.getParameterByName('li', src);
+        this.assert.equal(li, aData.LOGIN_ID);
 
-            events.forEach(function (key, i) {
-              if (src.indexOf('rt=' + key) >= 0) {
-                counts[key]++;
-                var test = tests[key].bind(THAT, client, src);
-                test();
-              }
-            });
+        var url = guiTest.getParameterByName('url', src);
+        this.assert.equal(url, DATA.URL);
+
+        var pg = guiTest.getParameterByName('pg', src);
+        this.assert.equal(pg, aData.CHANNEL_ID);
+
+        var vd = guiTest.getParameterByName('vd', src);
+        this.assert.equal(vd, aData.VIDS[current]);
+
+        client.waitForElementPresent('p.analtyticsTestVV', 6000);
+        client.expect.element('p.analtyticsTestVV').to.be.present;
+
+        var vvs = guiTest.getParameterByName('vvs', src);
+        this.assert.ok(vvs);
+
+        DATA.vvs = vvs;
+      }
+
+    };
+
+    client.frame(frame).elements("tag name", "script", function(result) {
+      console.log(">>> Widget Analytics Testing <<<");
+
+      result.value.forEach(function(script) {
+        client.elementIdAttribute(script.ELEMENT, 'src', function(res) {
+          var src = res.value,
+              THAT = this;
+          events.forEach(function (key, index) {
+            if (src.indexOf('rt=' + key) >= 0) {
+              var vd = guiTest.getParameterByName('vd', src),
+                  current = guiTest.inArray(vd, aData.VIDS, true);
+
+              aCounts[key]++;
+
+              var test = tests[key].bind(THAT, client, src, current);
+              test();
+            }
           });
         });
       });
-    }
+    });
+  },
+  getAnalyticCounts: function () {
+    return aCounts;
+  }
 
 };
 
@@ -141,15 +128,16 @@ module.exports = {
 
     guiTest.playVideo(client, DATA.SLA);
 
-    guiTest.analytics(client, 1,['ci','vv'], {LOGIN_ID:'1758799', CHANNEL_ID: '66133904', 'VIDEO_ID': '65981962' });
+    client.pause(6000);
 
-    client.pause(4000);
+    guiTest.playVideo(client, DATA.SLA);
 
-    // TODO: once solo auto next issue is fixed, then enable following
-
-    // guiTest.playVideo(client, DATA.SLA);
-
-    // guiTest.analytics(client, 1,['ci','vv'], {LOGIN_ID:'1758799', CHANNEL_ID: '66133904', 'VIDEO_ID': '83106081' });
+    guiTest.analytics(client, 1, ['ci', 'vv'], {
+      LOGIN_ID: 1758799,
+      CHANNEL_ID: 66133904,
+      VIDS: [65981962,83106081],
+      COUNTS: {"ci": 2, "vv": 2}
+    });
 
     client.end();
   }
